02daa6bf4e137b40d22647312bcd6b4695d23e28
Andre Schweighofer <dre@fidrelity.at>
2011-05-02 11:47:11 +0200

    pull merge

--- src/com/runnershigh/Info.java
+++ src/com/runnershigh/Info.java
@@ -9,10 +9,11 @@ public class Info extends Activity{
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	requestWindowFeature(Window.FEATURE_NO_TITLE);  
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
