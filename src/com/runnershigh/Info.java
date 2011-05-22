10f60fe44d8d836985c8c984bee924f29d10243d
Andre Schweighofer <dre@fidrelity.at>
2011-05-22 23:42:38 +0200

    info view not crashing anymore and landscape only/title removed from menu

--- src/com/runnershigh/Info.java
+++ src/com/runnershigh/Info.java
@@ -13,7 +13,7 @@ public class Info extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);  
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);  
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
